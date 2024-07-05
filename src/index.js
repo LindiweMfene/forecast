7574eede200ed4e3db362a26fc675b014fe03e33
LindiweMfene <lindiwe.e.mfene@gmail.com>
2024-07-05 14:26:13 +0200

    created JS file

---
+++ src/index.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById('background-video');
+    video.playbackRate = 0.5; // Set the playback rate to 0.5 for half speed
