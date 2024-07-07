941b36535ef61342e4876b3e127b13620764d01b
LindiweMfene <lindiwe.e.mfene@gmail.com>
2024-07-07 22:32:12 +0200

    added class to span

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     var video = document.getElementById('background-video');
-    video.playbackRate = 0.5; // Set the playback rate to 0.5 for half speed
+    video.playbackRate = 0.9; // Set the playback rate to 0.5 for half speed
+});
